21a5add7a59597cc5cbc33d9d0a581a44e41f654
jiadali <Jiada.Li@colostate.edu>
2022-12-08 23:03:29 -0700

    formatting salary data

---
+++ src/main/java/com/neutrinosys/peopledbweb/web/formatter/BigDecimalFormatter.java
@@ -0,0 +1,20 @@
+package com.neutrinosys.peopledbweb.web.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class BigDecimalFormatter implements Formatter<BigDecimal> {
+    @Override
+    public BigDecimal parse(String text, Locale locale) throws ParseException {
+        return null;
+    }
+
+    @Override
+    public String print(BigDecimal object, Locale locale) {
+        return NumberFormat.getCurrencyInstance(locale).format(object);
+    }
+}
